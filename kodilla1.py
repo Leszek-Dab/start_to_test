2da2649691748e16b9b187dde3424da08d7b2c8f
Leszek-Dab <leehu@wp.pl>
2020-06-21 13:30:58 +0200

    No to coś wrzucamy. Ciekwawe jak łyka polskie chary. Plik z prostym kodem pythonowym

---
+++ kodilla1.py
@@ -0,0 +1,13 @@
+def dodawanie():
+    return 2 + 5
+
+def pozdrowienia():
+    print("Niech Ci się darzy :D")
+
+
+print ("Wynik mojej funkcji dodawanie t0: ",dodawanie())
+
+x = "Leszek"
+y = 1985
+
+print("Cześć",x, "rok Twojego urodzenia to", y, ".")
